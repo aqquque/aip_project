1c94a02ac97365242ce93cc6d9a600951f27c056
aqquque <eretik.99.99@mail.ru>
2026-01-04 23:38:06 +0300

    пункт 4.2

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,14 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/lilpeep', function(req, res, next) {
+    res.send("<h1>Страница Lil peep</h1>")
+});
+router.get('/2hollis', function(req, res, next) {
+    res.send("<h1>Страница 2hollis</h1>")
+});
+router.get('/temniy-princ', function(req, res, next) {
+    res.send("<h1>Страница Тёмный принц</h1>")
+});
 
 module.exports = router;
