b4709803cdcbb383c75d56a537e5e9052911a0c6
aqquque <eretik.99.99@mail.ru>
2026-01-08 20:57:19 +0300

    tc_8_3

--- routes/musicians.js
+++ routes/musicians.js
@@ -1,16 +1,23 @@
-// routes/musicians.js
 var express = require('express');
 var router = express.Router();
+var Musician = require('../models/musician');
 
 /* GET musicians listing. */
 router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор для маршрутов, начинающихся с musicians');
+    res.send('Новый маршрутизатор, для маршрутов, начинающихся с musicians');
 });
 
-/* Страница конкретного музыканта по никнейму */
-router.get('/:nick', function(req, res, next) {
-  // Отображаем полученный параметр
-  res.send(`${req.params.nick}`);
+/* Страница музыкантов */
+router.get("/:nick", async function(req, res, next) {
+   var musicians = await Musician.find({nick: req.params.nick});
+   console.log(musicians)
+   if(!musicians.length) return next(new Error("Нет такого музыканта"))
+       var musician = musicians[0];
+       res.render('musician', {
+           title: musician.title,
+           picture: musician.avatar,
+           desc: musician.desc
+       })
 });
 
 module.exports = router;
